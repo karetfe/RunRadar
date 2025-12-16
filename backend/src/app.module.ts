1241a7fc6d9767402804545b7f06175cb243c40d
Mokhtari Afef <afef1.mokhtari@gmail.com>
2025-12-16 19:06:40 +0100

    config envi local, and connect NestJS to Mysql db with TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT ?? 3306),
+      username: process.env.DB_USER ?? 'runradar',
+      password: process.env.DB_PASSWORD ?? 'runradar',
+      database: process.env.DB8NAME ?? 'runradar',
+      autoLoadEntities: true,
+      synchronize: true, // OK en dev , pas en prod
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
